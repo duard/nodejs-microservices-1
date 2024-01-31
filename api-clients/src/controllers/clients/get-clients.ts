5f26843b5ec459c6ee21f12070de8cef2c002142
Carlos Developer <duard.developer@gmail.com>
2024-01-31 15:33:44 -0300

    apis se comunicando

--- api-clients/src/controllers/clients/get-clients.ts
+++ api-clients/src/controllers/clients/get-clients.ts
@@ -3,16 +3,54 @@ import logger from '../../logger';
 import { getRandonCompanyByQuantity } from '../../services/companies/company.service';
 import { getRandonPersonByQuantity } from '../../services/persons';
 
+interface Person {
+  firstname: string;
+  lastname: string;
+  // Add other person properties as needed
+}
+
+interface Company {
+  name: string;
+  email: string;
+  phone: string;
+  // Add other company properties as needed
+}
+
+interface Client {
+  name: string;
+  email: string;
+  phone: string;
+  person: {
+    fullName: string;
+    // Add other person properties as needed
+  };
+}
+
 const getClientsController = async (req: Request, res: Response): Promise<void> => {
   try {
-    console.log('buscando quantityCompanies');
     const quantityCompanies = parseInt(req.query.quantityCompanies as string, 10) || 1;
-    const quantityPersons = parseInt(req.query.quantityPersons as string, 10) || 1;
 
-    const companiesData = await getRandonCompanyByQuantity(quantityCompanies);
-    const personsData = await getRandonPersonByQuantity(quantityPersons);
+    const companiesData: any = await getRandonCompanyByQuantity(quantityCompanies);
 
-    res.json({ companies: companiesData, persons: personsData });
+    const clients: any[] = [];
+
+    await Promise.all(companiesData?.companies.map(async (company: Company) => {
+      const personsData: any = await getRandonPersonByQuantity(1);
+      logger.info(JSON.stringify(personsData));
+
+      const person: any = personsData?.persons[0];
+
+      clients.push({
+        name: company.name,
+        email: company.email,
+        phone: company.phone,
+        person: {
+          fullName: `${person.firstname} ${person.lastname}`
+        }
+      });
+    }));
+
+    res.json({ total: 20, clients });
   } catch (error) {
     logger.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
